481e86523de5f0c7e383ac3f23cbecf82615f4fa
Ifedayo Adekoya <47520140+dayorolands@users.noreply.github.com>
2024-07-26 11:13:11 +0100

    Initial Commit

---
+++ wk4/wk4App.swift
@@ -0,0 +1,17 @@
+//
+//  wk4App.swift
+//  wk4
+//
+//  Created by Dayo Adekoya on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct wk4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
